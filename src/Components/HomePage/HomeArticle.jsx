67d83533bbc9f52d5e3d18e5cfaf01acd132cd76
Mr. Kashan <168665220+kRehman007@users.noreply.github.com>
2024-09-16 14:16:13 -0700

    Update HomeArticle.jsx

--- src/Components/HomePage/HomeArticle.jsx
+++ src/Components/HomePage/HomeArticle.jsx
@@ -26,7 +26,7 @@ const HomeArticle = () => {
               <CiHeart className="text-3xl" />
               <FaRegComment className="text-2xl" />
             </div>
-            <CiBookmark className="text-2xl pr-3" />
+            <CiBookmark className="text-4xl pr-3" />
           </div>
           <span>4000 likes</span>
           <input
